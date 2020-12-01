2191da3cb545ef77e68f660516c1742d16cbe6bc
gfaraujo <guilherme.araujo@imd.ufrn.br>
2020-11-30 21:01:31 -0300

    a few new articles

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { MDXProvider } from '@mdx-js/react'
 import '../style/index.css'
 
 function MyApp ({ Component, pageProps })  {
-  console.log(pageProps)
+  
   return (
     <MDXProvider >
       <Component {...pageProps} />
